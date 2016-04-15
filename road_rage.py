930f263119ecf674c4d9f39bcd4e16e13dcd660f
JonathanKross <kross.jonathan+github@gmail.com>
2016-04-15 13:34:21 -0400

    We have 30 cars on a road with a view position, evenly distributed

--- road_rage.py
+++ road_rage.py
@@ -5,20 +5,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+class Simulation:
+
+    def __init__(self):
+        self.ticks = 0
+        self.road = Road()
+
+    def tick(self):
+        # all cars change speed
+        # all cars change position
+        pass
+
+    def sim(self, time = 60):
+        pass
+
+
 class Car:
-    def __init__(self, speed = ?, max_speed = ?, position = 0):
+
+    def __init__(self, max_speed=33, speed=0, position=0):
         self.max_speed = max_speed
         self.speed = speed
         self.position = position
 
-  def change_position(self):
+    def __str__(self):
+        return "Position= {}".format(self.position)
+
+    def __repr__(self):
+            return self.__str__()
+
+    def change_position(self):
         self.position += self.speed
         if self.position > 1000:
             self.position = self.position % 1000
         return self.position
 
 
+
 class Road:
+
     def __init__(self):
         self.cars = [Car() for _ in range(30)]
         self.graph = np.array([0 for _ in range(1100)])
@@ -28,7 +52,7 @@ class Road:
     def inital_car_position(self):
         """1000/30 = 33.33. Evenly space cars. ~28 meters between cars initally"""
         initiation = 0
-        for car in self.car:
+        for car in self.cars:
             car.position = initiation
             initiation += 33
 
@@ -39,27 +63,21 @@ class Road:
         #if distance large enough, but car.speed == max_speed
         #if distance not large enough... car.speed  -= 2?
         #if distance
+        pass
 
 
     def move_all_car_positions(self):
         pass
 
+
     def list_positions(self):
         pass
+
 
     def list_speeds(self):
         pass
 
-class Simulation:
 
-    def __init__(self):
-        self.ticks = 0
-        self.road = Road()
-
-
-    def tick(self):
-        all cars change speed
-        all cars change position
-
-    def sim(self, time = 60):
-        pass
+sim = Simulation()
+sim.road.inital_car_position()
+print(sim.road.cars)
